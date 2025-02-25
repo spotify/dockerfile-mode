7ce17e054eca4d56ca8bc1e4a6a0dbf58efd8d52
Drew Csillag <drew@thecsillags.com>
2025-02-25 10:27:22 -0500

    bump version number

--- dockerfile-mode.el
+++ dockerfile-mode.el
@@ -4,7 +4,7 @@
 ;; Package-Requires: ((emacs "24"))
 ;; Homepage: https://github.com/spotify/dockerfile-mode
 ;; URL: https://github.com/spotify/dockerfile-mode
-;; Version: 1.7
+;; Version: 1.9
 ;; Keywords: docker languages processes tools
 ;;
 ;; Licensed under the Apache License, Version 2.0 (the "License"); you may not
